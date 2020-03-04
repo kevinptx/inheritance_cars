d6728cf9e70dd5e5fde14ce382fc0530d621daba
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-04 12:59:16 -0800

    task 1.2 start

--- src/test/java/inheritance_cars/CarEngineTest.java
+++ src/test/java/inheritance_cars/CarEngineTest.java
@@ -1,8 +1,12 @@
+
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import inheritance_cars.*;
 
 class CarEngineTest {
 
@@ -30,35 +34,51 @@ class CarEngineTest {
 //        1. For each of the components outlined in the _Introduction_ section above, create a Java class. Begin by first doing this in a flat structure. Every class you write should have these methods:
 //
 //        1. `fullDescription` - returns a `String` containing all of the attributes of the instance of that class listed. For example an instance of `Radiator` would return something like: `"manufacturer: Nissen Automotive, partNumber: 17117507972, description: The full-aluminum radiators from Nissens are produced on the basis of the latest and most advanced technologies, including fully automatic manufacturing and assembly lines, coolantPressure: 250 Pa"`
-//        1. `coolingSystemComponent` - returns a boolean indicating whether or not an instance of the class is a cooling system
-//        component.
-//        1. `engineComponent` - returns a boolean indicating whether or not an instance of the class is an engine component.
-//
-//        2. Refactor your code to use inheritance. Add tests for any Base classes that are introduced in this refactoring.
-//
-//        ## Setup
-//
-//        This project will use the [IntelliJ IDE](https://www.jetbrains.com/idea/) and start from scratch with a fresh Gradle project.
-//
-//        #### Creating a test
-//
-//        To create your first test, right click on `main/java` and select "New -> Java Class". In the dialogue to name the class, enter the name of the Java class (implementation, not test) that you would like to create. Once you have made the Java class, hover over the name, click on the Yellow lightbulb and select "Create Test" like so:
-//
-//        ![](screenshots/create_test_menu.png)
-//
-//        In the dialogue be sure to select "JUnit 4" as the Testing Library:
-//
-//        ![](screenshots/junit4_test.png)
-//
-//        Click "OK" and you have now set up your first test file.
-//
-//        ## Stretch
-//
-//        - Add a field for `price` and methods for `taxExempt` and `subTotal` that apply to all Components. Engines will be tax exempt, other parts will not be.
-//        - Create a `Calculator` class which takes an ArrayList of Components and returns the total at a rate of 10%
-//        - Be sure to use [BigDecimal](http://www.javapractices.com/topic/TopicAction.do?Id=13) since we'll be dealing with money.
-//        - Be sure to use TDD, OO modelling best practices, and [streams](https://www.sitepoint.com/java-8-streams-filter-map-reduce/)
-//        - Should any classes be abstract? If so, what is [your strategy](http://stackoverflow.com/questions/7569444/how-to-test-abstract-class-in-java-with-junit) for testing them?
+    @Test
+    public void TestFullDescriptionsOnAllClasses() {
+        //Setup
+        CamShaft camShaft = new CamShaft();
+        CarEngine engine = new CarEngine();
+        CoolingSystem coolingSystem = new CoolingSystem();
+        DeiselEngine deiselEngine = new DieselEngine();
+        FanBlade fanBlade = new Fanblade();
+        PetrolEngine petrolEngine = new PetrolEngine();
+        Radiator radiator= new Radiator();
+        //Exercise
+        //Assert
+        assertTrue(true);
+        //Teardown
+    }
+
+    //        1. `coolingSystemComponent` - returns a boolean indicating whether or not an instance of the class is a cooling system
+    //        component.
+    //        1. `engineComponent` - returns a boolean indicating whether or not an instance of the class is an engine component.
+    //
+    //        2. Refactor your code to use inheritance. Add tests for any Base classes that are introduced in this refactoring.
+    //
+    //        ## Setup
+    //
+    //        This project will use the [IntelliJ IDE](https://www.jetbrains.com/idea/) and start from scratch with a fresh Gradle project.
+    //
+    //        #### Creating a test
+    //
+    //        To create your first test, right click on `main/java` and select "New -> Java Class". In the dialogue to name the class, enter the name of the Java class (implementation, not test) that you would like to create. Once you have made the Java class, hover over the name, click on the Yellow lightbulb and select "Create Test" like so:
+    //
+    //        ![](screenshots/create_test_menu.png)
+    //
+    //        In the dialogue be sure to select "JUnit 4" as the Testing Library:
+    //
+    //        ![](screenshots/junit4_test.png)
+    //
+    //        Click "OK" and you have now set up your first test file.
+    //
+    //        ## Stretch
+    //
+    //        - Add a field for `price` and methods for `taxExempt` and `subTotal` that apply to all Components. Engines will be tax exempt, other parts will not be.
+    //        - Create a `Calculator` class which takes an ArrayList of Components and returns the total at a rate of 10%
+    //        - Be sure to use [BigDecimal](http://www.javapractices.com/topic/TopicAction.do?Id=13) since we'll be dealing with money.
+    //        - Be sure to use TDD, OO modelling best practices, and [streams](https://www.sitepoint.com/java-8-streams-filter-map-reduce/)
+    //        - Should any classes be abstract? If so, what is [your strategy](http://stackoverflow.com/questions/7569444/how-to-test-abstract-class-in-java-with-junit) for testing them?
     @AfterEach
     void tearDown() {
     }
